6292faa48ad7b2fcc10c49e47ac4e95c2c54f139
Vera <xd54565852@gmail.com>
2025-10-15 20:13:25 +0800

    for.utf8

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
 		break
-	price = input('請輸入商品價格：')
+	price = int(input('請輸入商品價格：'))
 	# p =[]
 	# p.append(name) 小清單
 	# p.append(price) 小清單
@@ -12,3 +12,8 @@ print(products)
 
 for p in products:
 	print(p[0],'的價格是',p[1])
+
+with open('products.csv', 'w',encoding='utf-8') as f: #打開檔案寫入/encoding='utf-8'編碼
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0]+','+str(p[1]) +'\n') #f.write真正的寫入
